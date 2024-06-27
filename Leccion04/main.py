af936678beda495445bdf40751c0eddfbe573e20
Matias Antonelli <matiasantonelli6874@gmail.com>
2024-06-27 17:40:43 -0300

    Update main.py

--- Leccion04/main.py
+++ Leccion04/main.py
@@ -1,3 +1,13 @@
+
+
+for letra in "Holanda":
+    if letra == "a":
+        print(f'Letra encontrada: {letra}')
+        break
+    else:
+        print("FIN CICLO FOR") #opcional (es terrible)
+
+
 #for i in range(6):
 #   if i % 2 == 0:
 #      print(f'Valor: {i}')
