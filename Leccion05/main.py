d346fd773a77d7771b4105dd433bc5e8cf678cfe
Matias Antonelli <matiasantonelli6874@gmail.com>
2024-06-29 23:23:00 -0300

    set y mapa (diccionarios)

--- Leccion05/main.py
+++ Leccion05/main.py
@@ -83,24 +83,72 @@ frutaLista[0] = "Manzana"
 frutas = tuple(frutaLista)
 print("\n", frutas)
 
+print("-----------------------------SET----------------------")
+planetas = {"Marte", "Jupiter", "Venus"}
+print(planetas)
+#largo
+print(len(planetas))
+#contiene elemento
+print("Marte" in planetas)
+#agregar elemento
+planetas.add("Tierra")
+print(planetas)
+planetas.add("Jupiter")
+print(planetas)
+#eliminar elemento (remove contine excepcion)
+planetas.remove("Tierra")
+print(planetas)
+#eliminar sin que tenga excepcion si no se encuentra en el set
+planetas.discard("Jupiter")
+print(planetas)
+#limpiar el set
+planetas.clear()
+print(planetas)
+#eliminar el set del programa
+del planetas
 
+print("--------------------DICCIONARIOS (mapa)------------------------")
 
+diccionario = {
+    'IDE': "Integrated development Environment",
+    'OOP': "Object Oriented Programming",
+    'DBMS': 'Database Management System'
+}
+print(diccionario)
+#largo
+print(len(diccionario))
+# acceder a un elemento
+print(diccionario['IDE'])
+#otra forma
 
+print(diccionario.get('OOP'))
+#modificando elementos
+diccionario['IDE'] = "integrated development enviroment"
+print(diccionario)
+#recorrer elementos
+for termino, valor in diccionario.items():
+    print(termino,":", valor)
+for termino in diccionario.keys():
+    print(termino)
+for termino in diccionario.values():
+    print(termino)
 
+# comprobar elemento existente
 
+print("IDE" in diccionario)
+#agregar elemento
 
+diccionario["PK"] = "Primary Key"
+print(diccionario)
 
+#remover elemento
 
+diccionario.pop("OOP")
+print(diccionario)
 
+#limpiar diccionario
 
-
-
-
-
-
-
-
-
-
-
-
+diccionario.clear()
+print(diccionario)
+#eliminar diccionario
+del diccionario
