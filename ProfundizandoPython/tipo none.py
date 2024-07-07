9258d55bc5c83fe2ff215ee65e352437dc6c2d8a
Matias Antonelli <matiasantonelli6874@gmail.com>
2024-07-07 13:28:37 -0300

    REPL y None

---
+++ ProfundizandoPython/tipo none.py
@@ -0,0 +1,19 @@
+variable = None #es falsa
+
+if variable:
+    print('Se considera verdadera')
+else:
+    print('Es falsa')
+
+    '''
+    ocho formas de generar un bool false 
+    comilla simple o doble (cadena vacia)
+    Lista vacia
+    tupla vacia
+    diccionario vacio
+    entero = 0
+    flotante = 0
+    bool falso
+    tipo None
+    
+    '''
